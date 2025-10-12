9f6486699ac15cbf21a296998d807bfbd51fafd1
deanglane <dglaneto@gmail.com>
2025-10-12 09:20:38 -0400

    Designed and styled main home page hero section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.css";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -6,19 +7,34 @@ function Home() {
       <main className={"wrapper"}>
         {/* hero image */}
         <section className={styles.hero}>
-          <h1>On my day off, I like to make things.</h1>
-          <p>
-            Welcome to my cozy corner of the internet—where code simmers, photos
-            steep, and game ideas bubble like a fresh pour‑over.
-          </p>
-          <a href="#">Explore Projects</a>
-          <a href="#">Meet the Barista</a>
-          <div>
-            <div>
-              <p>image</p>
+          <div className={styles.intro}>
+            <h1>
+              On my day off, I like to <span>make things</span>.
+            </h1>
+            <p>
+              Welcome to my cozy corner of the internet—where code simmers,
+              photos steep, and game ideas bubble like a fresh pour‑over.
+            </p>
+            <div className={styles.call_to_action}>
+              <NavLink to="/projects" className={"button"}>
+                Explore Projects
+              </NavLink>
+              <NavLink to="/about" className={"button_transparent"}>
+                Meet the Barista
+              </NavLink>
             </div>
-            <div>
-              <p>description</p>
+          </div>
+
+          <div className={styles.feature_reel_container}>
+            <div className={styles.card_background}>
+              <div className={styles.card_foreground}>
+                <p>☕</p>
+                <p>
+                  A comfy hero visual goes here — maybe a looping reel of sites,
+                  games, and food shots.
+                </p>
+              </div>
+              <div className={styles.feature_desc}>feature description</div>
             </div>
           </div>
         </section>
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../routes/Projects.module.css";
 
 function Projects() {
   return (
