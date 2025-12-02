9dabebc2f37360ac3abdf28c72faf341b3146658
deanglane <dglaneto@gmail.com>
2025-12-02 06:36:55 -0500

    WIP - new featured.js and linked to slider

---
+++ src/data/featured.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Pokedex",
+    desc: "A pokemon pokedex gameboy built using custom css styles and Pokemon API",
+    image: pokedex,
+    alt: "A pokemon pokedex application",
+    button: "/projects",
+  },
+  {
+    id: 2,
+    title: "Flash It",
+    desc: "Build your own flash cards for study",
+    image: flashIt,
+    alt: "Tappy Plane Screenshot",
+    button: "/projects",
+  },
+  {
+    id: 3,
+    title: "Hangry Tummy",
+    desc: "My first Frontend Website project converting a pdf design into a fully functioning site",
+    image: hangryTummy,
+    alt: "Hangry Tummy website",
+    button: "/projects",
+  },
+  {
+    id: 4,
+    title: "More News",
+    desc: "read the latest articles",
+    image: "",
+    alt: "news headlines",
+    button: "/blog",
+  },
+];
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,41 +9,7 @@ import SwiperGallery from "../components/SwiperGallery.jsx";
 import pokedex from "../assets/images/screen-shots/pokemon_pokedex_app_new.png";
 import flashIt from "../assets/images/screen-shots/flash_it_new.png";
 import hangryTummy from "../assets/images/screen-shots/hangry_tummy.png";
-import { projects } from "../data/projects.js";
-const features = [
-  {
-    id: 1,
-    title: "Pokedex",
-    desc: "A pokemon pokedex gameboy built using custom css styles and Pokemon API",
-    image: pokedex,
-    alt: "A pokemon pokedex application",
-    button: "/projects",
-  },
-  {
-    id: 2,
-    title: "Flash It",
-    desc: "Build your own flash cards for study",
-    image: flashIt,
-    alt: "Tappy Plane Screenshot",
-    button: "/projects",
-  },
-  {
-    id: 3,
-    title: "Hangry Tummy",
-    desc: "My first Frontend Website project converting a pdf design into a fully functioning site",
-    image: hangryTummy,
-    alt: "Hangry Tummy website",
-    button: "/projects",
-  },
-  {
-    id: 4,
-    title: "More News",
-    desc: "read the latest articles",
-    image: "",
-    alt: "news headlines",
-    button: "/blog",
-  },
-];
+import { featured } from "../data/featured.js";
 
 function Home() {
   // const featuredSorted = [...projects]
@@ -94,7 +60,7 @@ function Home() {
                   onSlideChange={() => console.log("slider change")}
                   onSwiper={(swiper) => console.log(swiper)}
                 >
-                  {features.map((f) => (
+                  {featured.map((f) => (
                     <SwiperSlide key={f.id}>
                       <FeatureCard
                         title={f.title}
